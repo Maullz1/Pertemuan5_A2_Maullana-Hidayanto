7b07042f962038913021de91d4ff53bb0359c593
Maullz1 <142768609+Maullz1@users.noreply.github.com>
2023-09-11 16:03:08 +0700

    code3.py

--- code3.py
+++ code3.py
@@ -1,6 +1,6 @@
 a = 1
-var_i = 1
+b = 1
 while a<=7 :
-    for var_i in range(7, 77, 7) :
-        print("7 x", a, "=", var_i) 
+    for b in range(7, 77, 7) :
+        print("7 x", a, "=", b) 
         a+=1
